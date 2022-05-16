44b057912bd35ff6f83fb398c06ab68a2bd6035a
Konsta Nenonen <konstanenonen@protonmail.com>
2022-05-16 17:35:27 +0300

    Still trying to fix bottom navigation stability

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -1,9 +1,9 @@
 import { BottomNavigation } from "@mui/material";
 import { BottomNavigationAction } from "@mui/material";
-import HomeIcon from '@mui/icons-material/Home';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import InsightsIcon from '@mui/icons-material/Insights';
-import './nav.css';
+import HomeIcon from "@mui/icons-material/Home";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import InsightsIcon from "@mui/icons-material/Insights";
+import "./nav.css";
 
 interface BottomNavProps {
   setPage: (page: number) => void;
@@ -12,18 +12,22 @@ interface BottomNavProps {
 
 function BottomNav(props: BottomNavProps) {
   return (
-    <BottomNavigation
-      showLabels
-      value={props.page}
-      onChange={(event, newValue) => {
-        props.setPage(newValue);
-      }}
-      className="bottom-nav"
-    >
-      <BottomNavigationAction label="Etusivu" icon={<HomeIcon />} />
-      <BottomNavigationAction label="Suosikit" icon={<FavoriteBorderIcon />} />
-      <BottomNavigationAction label="Tilastot" icon={<InsightsIcon />} />
-    </BottomNavigation>
+    <div className="bottom-nav">
+      <BottomNavigation
+        showLabels
+        value={props.page}
+        onChange={(event, newValue) => {
+          props.setPage(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Etusivu" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label="Suosikit"
+          icon={<FavoriteBorderIcon />}
+        />
+        <BottomNavigationAction label="Tilastot" icon={<InsightsIcon />} />
+      </BottomNavigation>
+    </div>
   );
 }
 
