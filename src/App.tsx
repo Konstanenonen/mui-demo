e2ff99d16199cc2144d825a2fa59aa23a71b1dfe
Konsta Nenonen <konstanenonen@protonmail.com>
2022-05-16 16:48:57 +0300

    Added css reset with mui

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,25 @@ import TopDrawer from "./components/TopDrawer";
 import MainView from "./components/MainView/MainView";
 import { theme, darkTheme } from "./theme";
 import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   const [page, setPage] = useState<number>(0);
   const [darkMode, setDarkMode] = useState<any>(false);
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : theme}>
-      <div className="application">
-        <TopDrawer setDarkMode={setDarkMode} setPage={setPage} />
-        <div className="main-area">
-          <MainView page={page} setPage={setPage} />
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={darkMode ? darkTheme : theme}>
+        <div className="application">
+          <TopDrawer setDarkMode={setDarkMode} setPage={setPage} />
+          <div className="main-area">
+            <MainView page={page} setPage={setPage} />
+          </div>
+          <BottomNav page={page} setPage={setPage} />
         </div>
-        <BottomNav page={page} setPage={setPage} />
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
