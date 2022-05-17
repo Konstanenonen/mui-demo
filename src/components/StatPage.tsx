52d036ec69d30962cb81a4ece960151b2f544711
Konsta Nenonen <konstanenonen@protonmail.com>
2022-05-17 18:03:53 +0300

    Added info section to stat page

--- src/components/StatPage.tsx
+++ src/components/StatPage.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import Statics from './Statics';
-import Chart from '../images/cool-chart.png';
+import React from "react";
+import Statics from "./Statics";
+import Chart from "../images/cool-chart.png";
+import { Alert, AlertTitle, Box } from "@mui/material";
 
 interface customerScore {
   co2: number;
@@ -16,10 +17,21 @@ function StatPage(props: StatPageProps) {
   return (
     <>
       <h1>Tilastot</h1>
-      <img width="300" src={Chart} alt="Suora, joka kertoo kuinka paljon olet säästänyt luonnonvaroja verrattuna keskivertokuluttajaan." />
+      <img
+        width="300"
+        src={Chart}
+        alt="Suora, joka kertoo kuinka paljon olet säästänyt luonnonvaroja verrattuna keskivertokuluttajaan."
+      />
       <Statics customerScore={props.customerScore} />
+      <Box sx={{ pl: "60px", pr: "60px" }}>
+        <Alert severity="info">
+          <AlertTitle>Lisätietoa</AlertTitle>
+          Nämä luvut on laskettu verraten luonnonvarojen käyttöä siihen,
+          että olisit meiltä ostamiesi tuotteiden sijaan hankkinut tuotteet uusina.
+        </Alert>
+      </Box>
     </>
-  )
+  );
 }
 
 export default StatPage;
