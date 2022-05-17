ff4184e5b26427b5edec7bd60bcf8d8ad6af0879
Konsta Nenonen <konstanenonen@protonmail.com>
2022-05-17 13:54:20 +0300

    Added categroies to shop page

---
+++ src/components/ShopDrawer.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+type Anchor = "top" | "left" | "bottom" | "right";
+
+function ShopDrawer() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button sx={{backgroundColor: '#00A77E', color: 'white'}} onClick={toggleDrawer("top", true)}>{"Kategoriat"}</Button>
+      <SwipeableDrawer
+        anchor={"top"}
+        open={state["top"]}
+        onClose={toggleDrawer("top", false)}
+        onOpen={toggleDrawer("top", true)}
+      >
+        {list("top")}
+      </SwipeableDrawer>
+    </div>
+  );
+}
+
+export default ShopDrawer;
--- src/components/StorePage/StorePage.tsx
+++ src/components/StorePage/StorePage.tsx
@@ -1,6 +1,8 @@
+import { Pagination } from "@mui/material";
 import React, { useState } from "react";
 import ControlledSpeedDial from "../ControlledSpeedDial";
 import ReusableCard from "../ReusableCard";
+import ShopDrawer from "../ShopDrawer";
 import "./store.css";
 
 interface StorePageProps {
@@ -13,56 +15,58 @@ function StorePage(props: StorePageProps) {
   return (
     <>
       <h1>Verkkokauppa</h1>
+      <ShopDrawer />
       <div className="grid">
         <ReusableCard
           imageHeight={125}
           handleClick={() => setItemAmount((prevState) => prevState + 1)}
           imageName="bike"
-          title="Pyörä"
+          title="Maastopyörä"
           buttonText="Osta"
-          text="Hieno pyörä."
+          text="100 €"
         />
         <ReusableCard
           imageHeight={125}
           handleClick={() => setItemAmount((prevState) => prevState + 1)}
           imageName="bike"
-          title="Pyörä"
+          title="Maastopyörä"
           buttonText="Osta"
-          text="Hieno pyörä."
+          text="100 €"
         />
         <ReusableCard
           imageHeight={125}
           handleClick={() => setItemAmount((prevState) => prevState + 1)}
           imageName="bike"
-          title="Pyörä"
+          title="Maastopyörä"
           buttonText="Osta"
-          text="Hieno pyörä."
+          text="100 €"
         />
         <ReusableCard
           imageHeight={125}
           handleClick={() => setItemAmount((prevState) => prevState + 1)}
           imageName="bike"
-          title="Pyörä"
+          title="Maastopyörä"
           buttonText="Osta"
-          text="Hieno pyörä."
+          text="100 €"
         />
         <ReusableCard
           imageHeight={125}
           handleClick={() => setItemAmount((prevState) => prevState + 1)}
           imageName="bike"
-          title="Pyörä"
+          title="Maastopyörä"
           buttonText="Osta"
-          text="Hieno pyörä."
+          text="100 €"
         />
         <ReusableCard
           imageHeight={125}
           handleClick={() => setItemAmount((prevState) => prevState + 1)}
           imageName="bike"
-          title="Pyörä"
+          title="Maastopyörä"
           buttonText="Osta"
-          text="Hieno pyörä."
+          text="100 €"
         />
       </div>
+      <Pagination sx={{ mb: 5 }} count={10} color="primary" />
       <div className="bottom-right">
         <ControlledSpeedDial setItemAmount={setItemAmount} itemAmount={itemAmount} />
       </div>
